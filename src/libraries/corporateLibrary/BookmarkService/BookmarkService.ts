585ad437134a52e0b8dc74784af7ba8f1b7ee330
Ajit Panada <ajpanada@microsoft.com>
2020-11-27 00:02:03 +0530

    updated

--- src/libraries/corporateLibrary/BookmarkService/BookmarkService.ts
+++ src/libraries/corporateLibrary/BookmarkService/BookmarkService.ts
@@ -43,6 +43,12 @@ export class BookmarkService implements IBookmarkService {
             .then(
                 (client: AadHttpClient): void => {
                     this.bookmarksClient = client;
+                    Logger.log({
+                        message: "Created new instance of AadHttpClient in BookmarkService constructor",
+                        level: LogLevel.Info,
+                        data: this.bookmarksClient
+                    });
+                    Logger.writeJSON(this.bookmarksClient);
                 },
                 (err) => {
                     Logger.write("Exception ocurred in getting AadHttpClient instance", LogLevel.Error);
@@ -63,11 +69,15 @@ export class BookmarkService implements IBookmarkService {
         const apiUrl: string = `${AppConfiguration.Setting().ApiBaseUrl}/api/GetBookmarks?code=KLofLip41yhwRGLh52q9sabeoi7nJxpKVZ9Ds3OSQwtWJFPaV5mqyw==`;
 
         try {
+            Logger.write(`Calling GetBookmarks api for apiUrl: ${apiUrl}`, LogLevel.Info);
+
             // Get the response
             const response: HttpClientResponse = await this.bookmarksClient
                 .get(apiUrl, AadHttpClient.configurations.v1);
 
             if (response.ok) {
+                Logger.write(`Received bookmarks successfully with status ${response.status}`);
+
                 // Read the value from the JSON
                 const bookmarks: any = await response.json();
 
@@ -94,11 +104,15 @@ export class BookmarkService implements IBookmarkService {
         const apiUrl: string = `${AppConfiguration.Setting().ApiBaseUrl}/api/GetBookmarks/${id}?code=4anZC7EuJ4NCIZS4BNDSazGFaBpDHTFkYTcQZvMQHFfagsfsqan2kA==`;
 
         try {
+            Logger.write(`Calling GetBookmarksById api for apiUrl: ${apiUrl}`, LogLevel.Info);
+
             // Get the response
             const response: HttpClientResponse = await this.bookmarksClient
                 .get(apiUrl, AadHttpClient.configurations.v1);
 
             if (response.ok) {
+                Logger.write(`Received bookmark for Id: ${id} successfully with status ${response.status}`);
+
                 // Read the value from the JSON
                 const bookmark: any = await response.json();
 
@@ -123,6 +137,8 @@ export class BookmarkService implements IBookmarkService {
         const apiUrl: string = `${AppConfiguration.Setting().ApiBaseUrl}/api/AddBookmark?code=LoHqdQaRLf2mlYjf4L81jf1l4gwrnMkOivr6IrJs5Wi3Qs82GoOadw==`;
 
         try {
+            Logger.write(`Calling AddBookmark api for apiUrl: ${apiUrl}`, LogLevel.Info);
+
             // Setup the options with header and body
             const headers: Headers = new Headers({
                 'Content-type': 'application/json',
@@ -145,6 +161,8 @@ export class BookmarkService implements IBookmarkService {
                 .post(apiUrl, AadHttpClient.configurations.v1, postOptions);
 
             if (response.ok) {
+                Logger.write(`Added bookmark successfully with status ${response.status}`);
+
                 // Read the value from the response
                 const responseText: string = await response.text();
 
